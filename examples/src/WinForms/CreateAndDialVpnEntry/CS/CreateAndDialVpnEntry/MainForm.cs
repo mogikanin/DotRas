be6aed42d2b675442dbf9f28041edd72b982627e
mogikanin <mogikanin.tir@gmail.com>
2018-12-17 21:14:39 +0300

    Fix compilation errors for CreateAndDialVpnEntry example. Also fix some Resharper warnings.

--- examples/src/WinForms/CreateAndDialVpnEntry/CS/CreateAndDialVpnEntry/MainForm.cs
+++ examples/src/WinForms/CreateAndDialVpnEntry/CS/CreateAndDialVpnEntry/MainForm.cs
@@ -1,7 +1,7 @@
 ﻿using System;
+using System.Linq;
 using System.Net;
 using System.Windows.Forms;
-using DotRas;
 
 namespace DotRas.Samples.CreateAndDialVpnEntry
 {
@@ -13,13 +13,18 @@ namespace DotRas.Samples.CreateAndDialVpnEntry
         public const string EntryName = "VPN Connection";
 
         /// <summary>
+        /// Defines the path to phonebook file.
+        /// </summary>
+        private const string PHONE_BOOK_PATH = "phonebook.pbk";
+
+        /// <summary>
         /// Holds a value containing the handle used by the connection that was dialed.
         /// </summary>
-        private RasHandle handle = null;
+        private RasHandle handle;
 
         public MainForm()
         {
-            this.InitializeComponent();
+            InitializeComponent();
         }
 
         /// <summary>
@@ -29,16 +34,23 @@ namespace DotRas.Samples.CreateAndDialVpnEntry
         /// <param name="e">An <see cref="System.EventArgs"/> containing event data.</param>
         private void CreateEntryButton_Click(object sender, EventArgs e)
         {
-            // This opens the phonebook so it can be used. Different overloads here will determine where the phonebook is opened/created.
-            this.AllUsersPhoneBook.Open();
+            try
+            {
+                // This opens the phonebook so it can be used. Different overloads here will determine where the phonebook is opened/created.
+                AllUsersPhoneBook.Open(PHONE_BOOK_PATH);
 
-            // Create the entry that will be used by the dialer to dial the connection. Entries can be created manually, however the static methods on
-            // the RasEntry class shown below contain default information matching that what is set by Windows for each platform.
-            RasEntry entry = RasEntry.CreateVpnEntry(EntryName, IPAddress.Loopback.ToString(), RasVpnStrategy.Default,
-                RasDevice.GetDeviceByName("(PPTP)", RasDeviceType.Vpn));
+                // Create the entry that will be used by the dialer to dial the connection. Entries can be created manually, however the static methods on
+                // the RasEntry class shown below contain default information matching that what is set by Windows for each platform.
+                var rasDevice = RasDevice.GetDevices().FirstOrDefault(_ => _.Name.Contains("(PPTP)") && _.DeviceType == RasDeviceType.Vpn);
+                var entry = RasEntry.CreateVpnEntry(EntryName, IPAddress.Loopback.ToString(), RasVpnStrategy.Default, rasDevice);
 
-            // Add the new entry to the phone book.
-            this.AllUsersPhoneBook.Entries.Add(entry);           
+                // Add the new entry to the phone book.
+                AllUsersPhoneBook.Entries.Add(entry);
+            }
+            catch (Exception ex)
+            {
+                StatusTextBox.AppendText(ex.ToString());
+            }
         }
 
         /// <summary>
@@ -48,27 +60,27 @@ namespace DotRas.Samples.CreateAndDialVpnEntry
         /// <param name="e">An <see cref="System.EventArgs"/> containing event data.</param>
         private void DialButton_Click(object sender, EventArgs e)
         {
-            this.StatusTextBox.Clear();
+            StatusTextBox.Clear();
 
             // This button will be used to dial the connection.
-            this.Dialer.EntryName = EntryName;
-            this.Dialer.PhoneBookPath = RasPhoneBook.GetPhoneBookPath(RasPhoneBookType.AllUsers);
+            Dialer.EntryName = EntryName;
+            Dialer.PhoneBookPath = PHONE_BOOK_PATH;
 
             try
             {
                 // Set the credentials the dialer should use.
-                this.Dialer.Credentials = new NetworkCredential("Test", "User");
+                Dialer.Credentials = new NetworkCredential("Test", "User");
 
                 // NOTE: The entry MUST be in the phone book before the connection can be dialed.
                 // Begin dialing the connection; this will raise events from the dialer instance.
-                this.handle = this.Dialer.DialAsync();
+                handle = Dialer.DialAsync();
 
                 // Enable the disconnect button for use later.
-                this.DisconnectButton.Enabled = true;
+                DisconnectButton.Enabled = true;
             }
             catch (Exception ex)
             {
-                this.StatusTextBox.AppendText(ex.ToString());
+                StatusTextBox.AppendText(ex.ToString());
             }
         }
 
@@ -79,7 +91,7 @@ namespace DotRas.Samples.CreateAndDialVpnEntry
         /// <param name="e">An <see cref="DotRas.StateChangedEventArgs"/> containing event data.</param>
         private void Dialer_StateChanged(object sender, StateChangedEventArgs e)
         {
-            this.StatusTextBox.AppendText(e.State.ToString() + "\r\n");
+            StatusTextBox.AppendText(e.State + "\r\n");
         }
 
         /// <summary>
@@ -91,25 +103,25 @@ namespace DotRas.Samples.CreateAndDialVpnEntry
         {
             if (e.Cancelled)
             {
-                this.StatusTextBox.AppendText("Cancelled!");
+                StatusTextBox.AppendText("Cancelled!");
             }
             else if (e.TimedOut)
             {
-                this.StatusTextBox.AppendText("Connection attempt timed out!");
+                StatusTextBox.AppendText("Connection attempt timed out!");
             }
             else if (e.Error != null)
             {
-                this.StatusTextBox.AppendText(e.Error.ToString());
+                StatusTextBox.AppendText(e.Error.ToString());
             }
             else if (e.Connected)
             {
-                this.StatusTextBox.AppendText("Connection successful!");
+                StatusTextBox.AppendText("Connection successful!");
             }
 
             if (!e.Connected)
             {
                 // The connection was not connected, disable the disconnect button.
-                this.DisconnectButton.Enabled = false;
+                DisconnectButton.Enabled = false;
             }
         }
 
@@ -120,20 +132,18 @@ namespace DotRas.Samples.CreateAndDialVpnEntry
         /// <param name="e">An <see cref="System.EventArgs"/> containing event data.</param>
         private void DisconnectButton_Click(object sender, EventArgs e)
         {
-            if (this.Dialer.IsBusy)
+            if (Dialer.IsBusy)
             {
                 // The connection attempt has not been completed, cancel the attempt.
-                this.Dialer.DialAsyncCancel();
+                Dialer.DialAsyncCancel();
             }
             else
             {
                 // The connection attempt has completed, attempt to find the connection in the active connections.
-                RasConnection connection = RasConnection.GetActiveConnectionByHandle(this.handle);
-                if (connection != null)
-                {
-                    // The connection has been found, disconnect it.
-                    connection.HangUp();
-                }
+                var connection = RasConnection.GetActiveConnections().FirstOrDefault(_ => _.Handle == handle);
+
+                // The connection has been found, disconnect it.
+                connection?.HangUp();
             }
         }
     }
